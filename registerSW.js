35bc0525345be32cf07bfa8830294d58199d1356
Martin4287 <tu-correo@ejemplo.com>
2025-07-07 01:59:24 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/control-inventario-app/sw.js', { scope: '/control-inventario-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/-control-inventario-app-/sw.js', { scope: '/-control-inventario-app-/' })})}
